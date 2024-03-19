90e14beda767487d04a6fbfd75ec01a38d0ec742
BiggyTech <akinyemitestimonyiadedayo@gmail.com>
2024-03-20 00:19:51 +0530

    Add comparison function between computer and user
    the function compare if their are the same or
    different

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function getComputerChoice (){
 const choice = ["rock","paper","scissors"];
+const answer= prompt("choose between rock,paper,scissors");
 
+function getComputerChoice (){
 return choice[Math.floor(Math.random()*3)]
-
 }
-console.log(getComputerChoice ())
+console.log(getComputerChoice ());
+
+const comparison =(val)=>{
+ const computer= getComputerChoice();
+return  computer.localeCompare(val,"en-Us",{sensitivity:"base"})===0? 
+ `you win!. Computer choose ${computer}`: 
+ `you Lose!. Computer choose ${computer}`;
+}
+
+alert(comparison(answer))
